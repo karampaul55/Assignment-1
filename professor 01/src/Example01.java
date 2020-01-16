cfe0a7d0d0c7cbb4858f80d86011204b3cdf055d
Karam Paul <karampaul@me.com>
2020-01-15 22:02:47 -0500

    Modified Example

--- professor 01/src/Example01.java
+++ professor 01/src/Example01.java
@@ -10,7 +10,7 @@ public class Example01 {
 		System.out.println("Please enter your name: ");
 		String name = in.next();
 		
-		System.out.println("Hello " + name);
+		System.out.println("Hello " + name + "!");
 	}
 
 }
