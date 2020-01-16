5f39bcfba28d0608dc7ace84280e9d6e3053567a
Karam Paul <karampaul@me.com>
2020-01-15 22:56:52 -0500

    User Error Once Again

--- Assignment_1/src/GoodbyeWorld.java
+++ Assignment_1/src/GoodbyeWorld.java
@@ -3,7 +3,7 @@ public class GoodbyeWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Goodbye, World!");
+		System.out.printf("Goodbye, World!");
 	}
 
 }
